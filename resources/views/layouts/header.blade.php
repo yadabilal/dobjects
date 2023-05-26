8616812fb02f65c53fbbe264ff91781559f7235b
Bilal Yada <bilal.yada@beyn.com.tr>
2023-05-26 16:51:30 +0300

    mobile menu fixed

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,9 +4,7 @@
             <div class="section-container">
                 <div class="row">
                     <div class="col-xl-4 col-lg-4 col-md-4 col-sm-3 col-3 header-left">
-                        <div class="navbar-header">
-                            <button type="button" id="show-megamenu" class="navbar-toggle"></button>
-                        </div>
+
                     </div>
                     <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6 header-center">
                         <div class="site-logo">
@@ -34,6 +32,9 @@
             <!-- Shop -->
             <div class="shop-page">
                 <a href="{{route('home')}}"><i class="wpb-icon-shop"></i></a>
+            </div>
+            <div class="shop-page">
+                <a href="{{route('contact')}}"><i class="wpb-icon-chat"></i></a>
             </div>
             @guest
                 <!-- Login -->

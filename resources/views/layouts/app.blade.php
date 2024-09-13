2b21fc6a58547b44597dc6bdf3a11dc0ebb00eca
Bilal Yada <bilal.yada@beyn.com.tr>
2024-09-13 17:13:39 +0300

    fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,8 +59,9 @@
 @include('layouts.js')
 
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQNNZ55T"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQNNZ55T" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
 <!-- End Google Tag Manager (noscript) -->
 </body>
 </html>

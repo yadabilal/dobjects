a344e2bb2d8c54c2b4765a1f5c8e35b08151026c
Bilal Yada <bilal.yada@beyn.com.tr>
2024-09-16 21:04:32 +0300

    fixed

--- routes/web.php
+++ routes/web.php
@@ -66,9 +66,9 @@ Route::prefix('giris-yapmadan')->group(function () {
     Route::get('alisveris/odeme/{uuid}', 'GuestShopController@payment')->name('guest.shop.payment');
     Route::post('alisveris/odeme', 'GuestShopController@save_payment')->name('guest.shop.save_payment');
 
-    Route::post('alisveris/odeme/sonuc/{uuid}', 'GuestShopController\ShopController@callbackPayment')->name('guest.shop.callback');
-    Route::get('alisveris/odeme/sonuc/{uuid}', 'GuestShopController\ShopController@callbackPayment')->name('guest.shop.callback');
-    Route::get('alisveris/odeme/tamamlandi/{uuid}', 'GuestShopController\ShopController@resultPayment')->name('guest.shop.result');
+    Route::post('alisveris/odeme/sonuc/{uuid}', 'GuestShopController@callbackPayment')->name('guest.shop.callback');
+    Route::get('alisveris/odeme/sonuc/{uuid}', 'GuestShopController@callbackPayment')->name('guest.shop.callback');
+    Route::get('alisveris/odeme/tamamlandi/{uuid}', 'GuestShopController@resultPayment')->name('guest.shop.result');
 });
 
 
